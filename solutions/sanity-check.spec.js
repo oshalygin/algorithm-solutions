a6a7c3d6cc711491068ecc1e73d31b05e112df10
oshalygin <oshalygin@gmail.com>
2016-06-07 20:47:49 -0700

    a bit more meat

---
+++ solutions/sanity-check.spec.js
@@ -0,0 +1,7 @@
+(function () {
+    describe("sanity check tests", function () {
+        it("simple math", function () {
+            expect(1 + 1).toEqual(2);
+        });
+    });
+})();
--- wallaby.js
+++ wallaby.js
@@ -1,19 +1,17 @@
 module.exports = function (wallaby) {
     return {
-
         "files": [
             "Solutions/**/*.spec.js"
         ],
 
         "tests": [
             "Solutions/**/*.spec.js"
-
         ],
 
         testFramework: 'jasmine',
         debug: false,
         compilers: {
-            'Solutions/**/*.js': wallaby.compilers.babel({
+            'Solutions/**/*.spec.js': wallaby.compilers.babel({
                 babel: require("./node_modules/babel-core")
             })
         },
