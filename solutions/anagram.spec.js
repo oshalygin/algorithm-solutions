569d2239aec027d38dfaf74452c5505ea32e86b9
Oleg Shalygin <oshalygin@gmail.com>
2016-06-24 18:17:55 -0700

    a bit more work on the anagram solution

--- solutions/anagram.spec.js
+++ solutions/anagram.spec.js
@@ -1,6 +1,17 @@
+
 (() => {
 
     // describe("Anagram", () => {
+    Array.prototype.unique = function () {
+        var returnArray = [];
+
+        for (let element of this) {
+            if (!returnArray.includes(element)) {
+                returnArray.push(element);
+            }
+        }
+        return returnArray;
+    }
 
     function parseString(input) {
         var arrayOfStrings = input.split("\n");
@@ -9,7 +20,7 @@
     }
 
     function splitIntoTwoStrings(stringInput) {
-        let pairOfStrings = []
+        let pairOfStrings = [];
         let lengthOfString = stringInput.length;
         let firstString = stringInput.slice(0, lengthOfString / 2);
         let secondString = stringInput.slice(lengthOfString / 2, lengthOfString);
@@ -33,19 +44,47 @@
         return Math.abs(firstStringValue - secondStringValue);
     }
 
-    function largestStringValue(firstString, secondString) {
-        let firstStringValue = calculateStringCharacterValue(firstString);
-        let secondStringValue = calculateStringCharacterValue(secondString);
+    // function lowestStringValue(firstString, secondString) {
+    //     let firstStringValue = calculateStringCharacterValue(firstString);
+    //     let secondStringValue = calculateStringCharacterValue(secondString);
 
-        return (firstStringValue > secondStringValue)
-            ? firstStringValue
-            : secondStringValue;
+    //     return (firstStringValue > secondStringValue)
+    //         ? firstStringValue
+    //         : secondStringValue;
+    // }
+
+    function getArrayOfCharacterCodeValuesFromString(stringInput) {
+        let characterArray = stringInput.split("").map(x => x.charCodeAt(0)).unique();
+
+        return characterArray;
+    }
+
+    function getNumberOfCharacterSwitches(valueDifference, characters) {
+        const alreadyAnAnagram = -1;
+        if (valueDifference === 0) {
+            return alreadyAnAnagram;
+        }
+
+    }
+
+    function calculateAnagram(testCase){
+        let pairOfStrings = splitIntoTwoStrings(testCase);
+        let characterValueDifference = differenceBetweenTwoStrings(pairOfStrings[0], pairOfStrings[1]);
+        // let lowestStringValue = lowestStringValue(pairOfStrings[0], pairOfStrings[1]);
+    }
+
+
+    function calculateAnagramForTestCases(input) {
+        let arrayOfTestCases = parseString(input);
+        for (let testCase of arrayOfTestCases) {
+
+        }
     }
 
     //     it("Test Case: 6 aaabbb ab abc mnop xyyx xaxbbbxx", () => {
     const input = "6\n\aaabbb\n\nab\nabc\nmnop\nxyyx\nxaxbbbxx";
-    calculateStringCharacterValue("aabb");
-    console.log("derp");
+    let actual = calculateAnagram(input);
+
 
     //     });
     // });
