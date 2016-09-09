ec5dfaebf3691c56b64837e4ceec288309c0ba06
Oleg Shalygin <oshalygin@gmail.com>
2016-09-08 20:06:47 -0700

    eslint and new solution for parsed json

---
+++ solutions/parsed-json.spec.js
@@ -0,0 +1,9 @@
+(() => {
+    describe("Parsed JSON to Relational Array", () => {
+        const data = [{ "alternateName": null, "scheduledDateId": "a0Xi000000GIEzhEAH", "name": "MODERN FAMILY", "airTime": "09/07/2016", "startTime": "9:00:00 PM", "endTime": "9:31:00 PM", "seasonStartDate": "09/23/2015", "seasonEndDate": "09/21/2016", "DayOfWeek": "Wednesday" }, { "alternateName": null, "scheduledDateId": "a0Xi000000GIEziEAH", "name": "MODERN FAMILY", "airTime": "09/14/2016", "startTime": "9:00:00 PM", "endTime": "9:31:00 PM", "seasonStartDate": "09/23/2015", "seasonEndDate": "09/21/2016", "DayOfWeek": "Wednesday" }, { "alternateName": null, "scheduledDateId": "a0X170000029dRKEAY", "name": "MODERN FAMILY", "airTime": "09/21/2016", "startTime": "9:00:00 PM", "endTime": "9:31:00 PM", "seasonStartDate": "09/21/2016", "seasonEndDate": "10/12/2016", "DayOfWeek": "Wednesday" }, { "alternateName": null, "scheduledDateId": "a0X170000029dRLEAY", "name": "MODERN FAMILY", "airTime": "09/28/2016", "startTime": "9:00:00 PM", "endTime": "9:31:00 PM", "seasonStartDate": "09/21/2016", "seasonEndDate": "10/12/2016", "DayOfWeek": "Wednesday" }, { "alternateName": null, "scheduledDateId": "a0X170000029dRMEAY", "name": "MODERN FAMILY", "airTime": "10/05/2016", "startTime": "9:00:00 PM", "endTime": "9:31:00 PM", "seasonStartDate": "09/21/2016", "seasonEndDate": "10/12/2016", "DayOfWeek": "Wednesday" }, { "alternateName": null, "scheduledDateId": "a0X170000029dRNEAY", "name": "MODERN FAMILY", "airTime": "10/12/2016", "startTime": "9:00:00 PM", "endTime": "9:31:00 PM", "seasonStartDate": "09/21/2016", "seasonEndDate": "10/12/2016", "DayOfWeek": "Wednesday" }];
+
+        it("static json", () => {
+
+        });
+    });
+})();
