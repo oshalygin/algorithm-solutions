d029182e2b8bfd1026809eec3bf156b07890bbe2
Oleg Shalygin <oshalygin@gmail.com>
2016-10-10 23:38:54 -0700

    cleaning up formatting

--- solutions/very-big-sum.spec.js
+++ solutions/very-big-sum.spec.js
@@ -4,18 +4,19 @@
             return arrayInput.reduce((previousValue, newValue) => {
                 return previousValue + newValue;
             });
-        };
-
-        function convertToNumberArray(arrayOfStrings) {
-            return arrayOfStrings.map(returnIntegerValue);
         }
 
         function returnIntegerValue(number) {
             return parseInt(number, 10);
         }
 
+        function convertToNumberArray(arrayOfStrings) {
+            return arrayOfStrings.map(returnIntegerValue);
+        }
+
+
         it("Test Scenario: 1000000001 1000000002 1000000003 1000000004 1000000005 : 5000000015", () => {
-            let inputCapacityOfValues = "5";
+
             let inputStringOfValues = "1000000001 1000000002 1000000003 1000000004 1000000005";
             let expected = 5000000015;
 
@@ -23,7 +24,7 @@
             let arrayOfNumbers = convertToNumberArray(arrayOfStringValues);
 
             let actual = sum(arrayOfNumbers);
-            expect(actual).toEqual(expected);
+            expect(actual).toEqual(expected); //eslint-disable-line no-undef
         });
     });
 })();
