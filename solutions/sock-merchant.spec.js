00a4ff477e3ec38f30fe1f2b3bfaecdc53612ec2
oshalygin <oshalygin@gmail.com>
2017-01-11 22:37:39 -0800

    Add initial starting point for Sock Merchant

---
+++ solutions/sock-merchant.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+
+/*
+  John's clothing store has a pile of  loose socks where each sock  is labeled with an integer, , denoting its color. He wants to sell as many socks as possible, but his customers will only buy them in matching pairs. Two socks, and , are a single matching pair if .
+
+Given  and the color of each sock, how many pairs of socks can John sell?
+*/
+
+// import { expect } from 'chai';
+(() => {
+  describe('Sock Merchant', () => {
+    it('Simple case: 10 20 20 10 10 30 50 10 20 => 3', () => {
+
+    });
+  });
+})();
