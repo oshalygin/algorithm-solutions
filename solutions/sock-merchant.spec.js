38c5e0a6a65a8206de46b99232fb6aa7cda49be9
oshalygin <oshalygin@gmail.com>
2017-01-11 23:31:52 -0800

    Add Sock Merchant solution

--- solutions/sock-merchant.spec.js
+++ solutions/sock-merchant.spec.js
@@ -6,10 +6,58 @@
 Given  and the color of each sock, how many pairs of socks can John sell?
 */
 
-// import { expect } from 'chai';
+import { expect } from 'chai';
 (() => {
   describe('Sock Merchant', () => {
+
+    function getPairs(pairCount, workingSet) {
+
+      if (workingSet.length === 0) {
+        return pairCount;
+      }
+
+      const pairValue = workingSet.shift();
+      const matchingPairIndex = workingSet.indexOf(pairValue);
+      if (matchingPairIndex !== -1) {
+        workingSet.splice(matchingPairIndex, 1);
+        return getPairs(pairCount + 1, workingSet);
+      }
+      return getPairs(pairCount, workingSet);
+    }
+
     it('Simple case: 10 20 20 10 10 30 50 10 20 => 3', () => {
+      const input = '10 20 20 10 10 30 50 10 20';
+      const expected = 3;
+      const socks = input.split(' ').map(Number);
+
+      const initialPairCount = 0;
+      const actual = getPairs(initialPairCount, [...socks]);
+
+      expect(actual).equals(expected);
+
+    });
+
+    it('Simple case: 10 20 20 10 10 30 50 10 50 => 4', () => {
+      const input = '10 20 20 10 10 30 50 10 50';
+      const expected = 4;
+      const socks = input.split(' ').map(Number);
+
+      const initialPairCount = 0;
+      const actual = getPairs(initialPairCount, [...socks]);
+
+      expect(actual).equals(expected);
+
+    });
+
+    it('Simple case: 30 30 30 30 30 30 30 30 30 30 30 30 => 6', () => {
+      const input = '30 30 30 30 30 30 30 30 30 30 30 30';
+      const expected = 6;
+      const socks = input.split(' ').map(Number);
+
+      const initialPairCount = 0;
+      const actual = getPairs(initialPairCount, [...socks]);
+
+      expect(actual).equals(expected);
 
     });
   });
